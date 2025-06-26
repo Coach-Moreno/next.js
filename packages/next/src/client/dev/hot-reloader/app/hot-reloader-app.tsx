d4d6a4f90d0e514642037930cf8f30be556c2382
RobPruzan <rapruzan@buffalo.edu>
2025-06-25 18:34:26 -0700

    include source location in normal log

--- packages/next/src/client/dev/hot-reloader/app/hot-reloader-app.tsx
+++ packages/next/src/client/dev/hot-reloader/app/hot-reloader-app.tsx
@@ -454,7 +454,6 @@ export default function HotReload({
   children: ReactNode
   globalError: [GlobalErrorComponent, React.ReactNode]
 }) {
-  console.log('hot reload')
 
   // oh something something
   useErrorHandler(dispatcher.onUnhandledError, dispatcher.onUnhandledRejection)
